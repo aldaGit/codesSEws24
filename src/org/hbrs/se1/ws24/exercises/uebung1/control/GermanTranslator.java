99c0e9a891a95605d9163cf13f700cae761fc59b
saschaalda <sascha.alda@h-brs.de>
2024-09-24 15:00:36 +0200

    First Version Uebung Nr. 1

---
+++ src/org/hbrs/se1/ws24/exercises/uebung1/control/GermanTranslator.java
@@ -0,0 +1,31 @@
+package org.hbrs.se1.ws24.exercises.uebung1.control;
+
+public class GermanTranslator implements Translator {
+
+	public String date = "Okt/2024"; // Default-Wert
+
+	/**
+	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
+	 */
+	public String translateNumber( int number ) {
+		// [ihr Source Code aus Übung 1-2]
+
+		return "null";
+	}
+
+	/**
+	 * Objektmethode der Klasse GermanTranslator zur Ausgabe einer Info.
+	 */
+	public void printInfo(){
+		System.out.println( "GermanTranslator v1.9, erzeugt am " + this.date );
+	}
+
+	/**
+	 * Setzen des Datums, wann der Uebersetzer erzeugt wurde (Format: Monat/Jahr (Beispiel: "Okt/2022"))
+	 * Das Datum sollte system-intern durch eine Factory-Klasse gesetzt werden und nicht von externen View-Klassen
+	 */
+	public void setDate( String date ) {
+		this.date = date;
+	}
+
+}
