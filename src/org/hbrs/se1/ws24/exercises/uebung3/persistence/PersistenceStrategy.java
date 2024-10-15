3c47458156eca5905c0b5470fc922283982264d3
saschaalda <sascha.alda@h-brs.de>
2024-10-15 15:01:27 +0200

    Neuer Code für die Übung Nr. 2

---
+++ src/org/hbrs/se1/ws24/exercises/uebung3/persistence/PersistenceStrategy.java
@@ -0,0 +1,15 @@
+package org.hbrs.se1.ws24.exercises.uebung3.persistence;
+
+import java.util.List;
+
+/**
+ * Interface for defining basic methods for a persistence mechanism
+ * Each concrete algorithm (i.e. strategy) must implement this method
+ * This interface corresponds to the abstract strategy w.r.t. to the
+ * Strategy Design Pattern (GoF).
+ *
+ */
+public interface PersistenceStrategy<E> {
+    public void save(List<E> member) throws PersistenceException;
+    public List<E> load() throws PersistenceException;
+}
---
+++ src/org/hbrs/se1/ws24/exercises/uebung3/persistence/PersistenceStrategyMongoDB.java
@@ -0,0 +1,16 @@
+package org.hbrs.se1.ws24.exercises.uebung3.persistence;
+
+import java.util.List;
+
+public class PersistenceStrategyMongoDB<E> implements PersistenceStrategy<E> {
+
+    @Override
+    public void save(List<E> member) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    @Override
+    public List<E> load() {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+}
