ec9e14adc327fd589229a1d977bff4dd8163dc66
saschaalda <sascha.alda@h-brs.de>
2025-01-07 14:47:15 +0100

    Refactoring der Klasse

--- src/org/hbrs/se1/ws24/demo/kapitel6/BuchungDAO.java
+++ src/org/hbrs/se1/ws24/demo/kapitel6/BuchungDAO.java
@@ -1,12 +1,20 @@
 package org.hbrs.se1.ws24.demo.kapitel6;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * CRUD-basiertes Interface für die Verwaltung von Buchungs-Objekten (vgl. Aufgabe 9-2c)
  */
 public class BuchungDAO {
     // Persistente Erzeugung und Speicherung eines Objekts vom Typ Buchung
     void createBuchung( Buchung buchung ) {}
+    Buchung createBuchung( BuchungDTO buchungDTO ) { return null; }
 
     // Weitere Methoden gemäß CRUD? [Todo]
+    void deleteBuchung( Buchung buchung ) {}
+    void updateBuchung( Buchung buchung ) {}
+    Buchung getBuchung( int id ) { return null; };
+    List<Buchung> getBuchungByDate( Date date ) { return null; }
 
 }
